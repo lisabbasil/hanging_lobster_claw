f2aa911c6d3379c9b028533bcbe63963e4949178
Basil Schneider <basil.schneider@cern.ch>
2018-10-18 01:22:35 +0200

    Add DataAccess class

---
+++ DataAccess.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+""" Module to read and write json data from project Alperose. """
+
+from json import load
+from logger import logging
+from pprint import pformat
+
+
+class DataAccess:
+
+    """ Class that provides read and write functions to access json data. """
+
+    def __init__(self, filename):
+        self._filename = filename
+        logging.debug('Initialize DataAccess object for reading json file %s' %
+                      filename)
+
+    def read(self):
+
+        """ Read from json file. """
+
+        logging.debug('Read json data from %s' % self._filename)
+        with open(self._filename) as json:
+            data = load(json)
+
+        # Use pprint to format json data nicely
+        logging.info('Retrieve json data:')
+        logging.info(pformat(data))
+        return data
+
+    def write(self):
+
+        """ Write to json file. """
+
+        pass
